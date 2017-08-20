4936159a09dfbce24fc83ddf74b831c51f2480fd
Halim <halim.bouguedra@gmail.com>
2017-08-20 14:27:30 +0100

    npm / webpack initial setup

---
+++ webpack.config.js
@@ -0,0 +1,45 @@
+module.exports = {
+	entry: __dirname + "/index.js",
+	output: {
+		filename: "bundle.js",
+		//publicPath: "/pottery/",
+		path: __dirname + "/build/"
+	},
+
+	devServer: {
+		contentBase: __dirname + "/build/",
+		port: 8080
+	},
+
+	module: {
+		loaders : [
+			{
+				test: /\.js$/,
+				exclude: /node_modules/,
+				loader: "babel-loader"
+			},
+
+			{
+				test: /\.(scss|sass)$/,
+				exclude: /node_modules/,
+				loader: "style-loader!css-loader!sass-loader"
+			},
+
+			{
+				test: /\.(png|svg)$/, 
+				exclude: /node_modules/,
+				loader: "url-loader"
+			},
+			
+			{
+                test: /\.(eot|svg|ttf|woff|woff2)$/,
+                loader: "file-loader?name=public/fonts/[name].[ext]"
+            },
+
+            {
+            	test: /\.json$/,
+            	loader: "json-loader"
+            }
+		]
+	}
+}
