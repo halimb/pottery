98f4b7e5a82b7c2d38d8da34a6f0229d241ea986
Halim <halim.bouguedra@gmail.com>
2017-08-23 15:21:12 +0100

    import sass variables via shadow html elements

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,11 +1,29 @@
 import "../styles/style.scss";
 import Masonry from "masonry-layout";
 
+var a, item, gutter, fullItem;
 
+//Initialize variables
+(
+	() => {
+		var getWidth = s => {
+			return document
+					.querySelector(s)
+					.offsetWidth;
+		}
+		a = getWidth(".a");
+		item = getWidth(".item");
+		gutter = getWidth(".gutter");
+		fullItem = item + 2 * gutter;
+	}
+) ();
 
+//Setup Masonry
 var grid = document.querySelector('.grid');
 var msnry = new Masonry( grid, {
   // options...
   itemSelector: '.grid-item',
-  columnWidth: 220
+  columnWidth: fullItem
 });
+
+console.log(fullItem)
